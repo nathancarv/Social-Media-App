5d5ad1f23d15bece2699a054b4bceb66a1fc8bee
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-07-03 13:10:47 -0400

    testing w postman

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello World!");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('screams').get()
+    .then((data)=>{
+        let screams = [];
+        data.forEach((doc) =>{
+            screams.push(doc.data());
+        });
+        return res.json(screams);
+    })
+        .catch((err) => console.error(err));
 });
