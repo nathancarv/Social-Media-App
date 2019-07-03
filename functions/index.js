a6a2fa139051c438128b314654fc00cfa7aa3f29
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-07-02 21:13:14 -0400

    fixed syntax

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,25 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
+  response.send("Hello World!");
 });
 
 exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get()
-    .then((data)=>{
-        let screams = [];
-        data.forEach((doc) =>{
-            screams.push(doc.data());
-        });
-        return res.json(screams);
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
     })
-        .catch((err) => console.error(err));
+    .catch(err => console.error(err));
 });
