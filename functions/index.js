4a94fdf14dcafc7502dbddc51df0407c3156a6d1
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-07-06 18:49:04 -0400

    installed express

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,10 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
-});
+const express = require('express');
+const app = express();
 
 exports.getScreams = functions.https.onRequest((req, res) => {
   admin
