a06d05031cd8621aa91cb2d3b29824166c2227d2
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-07-08 13:20:11 -0400

    edited get

--- functions/index.js
+++ functions/index.js
@@ -3,46 +3,47 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-app.get('/screams',(req, res)=>{
-    admin
+app.get("/screams", (req, res) => {
+  admin
     .firestore()
     .collection("screams")
+    .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(screams);
     })
     .catch(err => console.error(err));
-  });
+});
 
-
-app.post('/scream', (req, res) => {
-  if (req.method !== 'POST'){
-      return res.status(400).json({error: "Method not allowed"});
-  }
-    const newScream = {
+app.post("/scream", (req, res) => {
+  const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
     createdAt: admin.firestore.Timestamp.fromDate(new Date())
   };
   admin
     .firestore()
-    .collection('screams')
+    .collection("screams")
     .add(newScream)
     .then(doc => {
-      res.json({ message: `document ${doc.id} created successfully`});
+      res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch(err => {
       res.status(500).json({ error: "something went wrong" });
       console.error(err);
     });
 });
- 
+
 exports.api = functions.https.onRequest(app);
